8528bdfc804cf7837f2a019707a268ee5b126406
Jack Kamaryt <jkamaryt46@gmail.com>
2023-04-06 11:16:42 -0500

    Initial Commit

---
+++ Car Hud/Car_HudApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_HudApp.swift
+//  Car Hud
+//
+//  Created by Jack Kamaryt on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Car_HudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
